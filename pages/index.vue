2384c7e3f19f55669e02b1c42416147a3df2f5f9
KazusaNakagawa <kazusanakgawa@gmail.com>
2020-12-29 13:32:00 +0900

    ファイル内で表示させていた箇所を削除

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="container">
     <div class="number">{{ count }}</div>
-    <!-- <v-btn class="mx-1" fab dark color="blue" @click="plus">
-      <v-icon dark> mdi-plus </v-icon>
-    </v-btn>
-    <v-btn class="mx-1" fab dark color="orange" @click="minus">
-      <v-icon dark> mdi-minus </v-icon>
-    </v-btn> -->
     <counter-button kind="plus" color="blue" @click="plus" />
     <counter-button kind="minus" color="orange" @click="minus" />
   </div>
