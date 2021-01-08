e03d75357c805405e7941912ce5b0ec616601533
KazusaNakagawa <kazusanakgawa@gmail.com>
2021-01-08 22:33:23 +0900

    文字列表示/非表示

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-show="isShown">みえる??</div>
+    <button @click="isView">見えるボタン</button>
+    <button @click="isNotView">見えないボタン</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShown: true,
+    }
+  },
+  methods: {
+    isView() {
+      this.isShown = true
+    },
+    isNotView() {
+      this.isShown = false
+    },
+  },
+}
+</script>
