1a6980a861a36b912724d0105c5ece394369da1e
KazusaNakagawa <kazusanakgawa@gmail.com>
2021-01-09 00:06:48 +0900

    v-show, v-btn で表示/非表示

---
+++ pages/test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- v-if 関係-->
+    <div v-if="score >= 80">よくできました！</div>
+    <div v-else-if="score >= 30">もっといけるで！</div>
+    <div v-else>がんばらんと！</div>
+    <hr />
+    <!-- v-show で表示/非表示-->
+    <div v-show="isShown">みえる??</div>
+    <!-- v-btn vueで用意されているボタンなんかいい感じ -->
+    <v-btn @click="toggleShown">表示/非表示の切り替え</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShown: true,
+      score: 40,
+    }
+  },
+  methods: {
+    // toggle: 訳/切り替える
+    toggleShown() {
+      this.isShown = !this.isShown
+    },
+  },
+}
+</script>
