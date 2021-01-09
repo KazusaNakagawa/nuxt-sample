6502f4df8d8fff54b787a27bcdc0e67cccb06c78
KazusaNakagawa <kazusanakgawa@gmail.com>
2021-01-09 16:20:58 +0900

    ライフサイクル SPA create

--- pages/lifecycle.vue
+++ pages/lifecycle.vue
@@ -4,6 +4,13 @@
 
     <v-text-field ref="number" v-model="number" />
     <div>数値: {{ number }}</div>
+
+    <div v-for="user in users" :key="user.id" :class="$style.container">
+      <!-- ここは要素を<div>で指定している -->
+      <div>name: {{ user.name }}</div>
+      <div>email: {{ user.email }}</div>
+      <div>phone: {{ user.phone }}</div>
+    </div>
   </div>
 </template>
 
@@ -12,10 +19,18 @@ export default {
   data() {
     return {
       number: 0,
+      users: [],
     }
   },
   created() {
     console.log('created')
+
+    this.$axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        // ここで debug response の値を確認して必要な要素を temolate で表示させる
+        this.users = response.data
+      })
   },
 
   mounted() {
@@ -36,4 +51,11 @@ export default {
 }
 </script>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.container {
+  margin: 10px 0;
+  padding: 8px;
+  border: 2px solid #da8;
+  border-radius: 4px;
+}
+</style>
